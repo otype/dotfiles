d916df06ab66a428bcac5618bc4e2a4bb811d1fe
Hans-Gunther Schmidt <hans@otype.de>
2016-04-13 23:26:44 +0200

    Add multi-config setup for gmail accounts

--- emacs.d/email.el
+++ emacs.d/email.el
@@ -1,104 +1,86 @@
 (add-to-list 'load-path "/usr/local/Cellar/mu/HEAD/share/emacs/site-lisp/mu/mu4e")
 (setq mu4e-mu-binary (executable-find "/usr/local/bin/mu"))
 (require 'mu4e)
+(require 'mu4e-maildirs-extension)
+(mu4e-maildirs-extension)
 
-(setq mu4e-maildir (expand-file-name "~/.mails/hansotypede")
-       mu4e-drafts-folder "/[Gmail].Drafts"
-       mu4e-sent-folder   "/[Gmail].Sent Mail"
-       mu4e-trash-folder  "/[Gmail].Trash"
-       mu4e-refile-folder "/[Gmail].All Mail"
-       message-signature-file "~/.signature-hansotypede"
-       mu4e-sent-messages-behavior 'delete
-       mu4e-get-mail-command "/usr/local/bin/mbsync -a"
-       mu4e-maildir-shortcuts
-       '( ("/INBOX"               . ?i)
- 	 ("/[Gmail].Sent Mail"   . ?s)
- 	 ("/[Gmail].Trash"       . ?t)
- 	 ("/[Gmail].Drafts"      . ?d)
- 	 ("/[Gmail].All Mail"    . ?a))
-       user-mail-address "hans@otype.de"
-       user-full-name  "Hans-Gunther Schmidt"
-       ;;mu4e-compose-signature
-       ;;(concat
-       ;; "Cheers,\n"
-       ;; "Hans\n")
+(setq mu4e-maildir (expand-file-name "~/.mails")
+      mu4e-html2text-command "/usr/local/bin/w3m -T text/html"
+      user-full-name "Hans-Gunther Schmidt"
+      mu4e-show-images t
+      mu4e-view-show-images t
+      mu4e-sent-messages-behavior 'delete
+      mu4e-get-mail-command "/usr/local/bin/mbsync -a"
+      mu4e-update-interval 600
+      message-send-mail-function 'message-send-mail-with-sendmail
+      sendmail-program "/usr/local/bin/msmtp"
+      message-sendmail-extra-arguments '("--read-envelope-from")
+      message-sendmail-f-is-evil 't
+      mu4e-use-fancy-chars t
+      mu4e-attachment-dir "~/Downloads")
 
-       mu4e-show-images t
-       mu4e-view-show-images t
-       mu4e-use-fancy-chars t
-       mu4e-attachment-dir "~/Downloads"
-       mu4e-html2text-command "/usr/local/bin/w3m -T text/html"
-       mu4e-update-interval 600
-       message-send-mail-function 'message-send-mail-with-sendmail
-       sendmail-program "/usr/local/bin/msmtp"
-       message-sendmail-extra-arguments '("--read-envelope-from")
-       message-sendmail-f-is-evil 't)
+(setq user-mail-address "hans@otype.de"
+      message-signature-file "~/.signature-hansotypede"
+      mu4e-sent-folder "/hansotypede/sent"
+      mu4e-drafts-folder "/hansotypede/drafts"
+      mu4e-trash-folder "/hansotypede/trash"
+      mu4e-refile-folder "/hansotypede/archive"
+      mu4e-maildir-shortcuts
+      '(
+	("/hansotypede/inbox"   . ?i)
+	("/hansotypede/sent"    . ?s)
+	("/hansotypede/trash"   . ?t)
+	("/hansotypede/drafts"  . ?d)
+	("/hansotypede/archive" . ?a)
+	("/hansschmidtmeltwatercom/inbox"   . ?I)
+	("/hansschmidtmeltwatercom/sent"    . ?S)
+	("/hansschmidtmeltwatercom/trash"   . ?T)
+	("/hansschmidtmeltwatercom/drafts"  . ?D)
+	("/hansschmidtmeltwatercom/archive" . ?A)))
+
+(defvar my-mu4e-account-alist
+  '(("hansotypede"
+     (user-mail-address "hans@otype.de")
+     (message-signature-file "~/.signature-hansotypede")
+     (mu4e-sent-folder "/hansotypede/sent")
+     (mu4e-drafts-folder "/hansotypede/drafts")
+     (mu4e-trash-folder "/hansotypede/trash")
+     (mu4e-refile-folder "/hansotypede/archive"))
+    ("hansschmidtmeltwatercom"
+     (user-mail-address "hans.schmidt@meltwater.com")
+     (message-signature-file "~/.signature-hansschmidtmeltwatercom")
+     (mu4e-sent-folder "/hansschmidtmeltwatercom/sent")
+     (mu4e-drafts-folder "/hansschmidtmeltwatercom/drafts")
+     (mu4e-trash-folder "/hansschmidtmeltwatercom/trash")
+     (mu4e-refile-folder "/hansschmidtmeltwatercom/archive"))))
+
+(defun my-mu4e-set-account ()
+  "Set the account for composing a message."
+  (let* ((account
+          (if mu4e-compose-parent-message
+              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                (string-match "/\\(.*?\\)/" maildir)
+                (match-string 1 maildir))
+            (completing-read (format "Compose with account: (%s) "
+                                     (mapconcat #'(lambda (var) (car var))
+                                                my-mu4e-account-alist "/"))
+                             (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+                             nil t nil nil (caar my-mu4e-account-alist))))
+         (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+        (mapc #'(lambda (var)
+                  (set (car var) (cadr var)))
+              account-vars)
+      (error "No email account found"))))
+
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+
+;; spell check
+(add-hook 'mu4e-compose-mode-hook
+	  (defun my-do-compose-stuff ()
+	    "My settings for message composition."
+	    (set-fill-column 72)
+	    (flyspell-mode)))
 
 (when (fboundp 'imagemagick-register-types)
   (imagemagick-register-types))
-
-;; ;; spell check
-;; (add-hook 'mu4e-compose-mode-hook
-;;          (defun my-do-compose-stuff ()
-;;             "My settings for message composition."
-;;             (set-fill-column 72)
-;;             (flyspell-mode)))
-
-;;  ;; add option to view html message in a browser
-;;  ;; `aV` in view to activate
-;;  (add-to-list 'mu4e-view-actions
-;;    '("ViewInBrowser" . mu4e-action-view-in-browser) t)
-
-;; (setq mu4e-contexts
-;;       `( ,(make-mu4e-context
-;;  	   :name "Private"
-;;  	   :enter-func (lambda () (mu4e-message "Switch to the Private context"))
-;;  	   ;; leave-func not defined
-;;  	   :match-func (lambda (msg)
-;;  			 (when msg 
-;;  			   (mu4e-message-contact-field-matches msg 
-;;  							       :to "hans@otype.de")))
-;;  	   :vars '(  ( user-mail-address . "hans@otype.de"  )
-;;  		     ( user-full-name	 . "Hans-Gunther Schmidt" )
-;; 		     ( mu4e-maildir (expand-file-name "~/.mails/hansotypede"))
-;; 		     ( mu4e-drafts-folder "/[Gmail].Drafts")
-;; 		     ( mu4e-sent-folder   "/[Gmail].Sent Mail")
-;; 		     ( mu4e-trash-folder  "/[Gmail].Trash")
-;; 		     ( mu4e-refile-folder "/[Gmail].All Mail")
-;; 		     ;; ( message-signature-file "~/.emacs.d/.signature")
-;; 		     ( mu4e-sent-messages-behavior 'delete)
-;; 		     ;; ( mu4e-get-mail-command "/usr/local/bin/mbsync -a")
-;; 		     ;; ( mu4e-maildir-shortcuts
-;; 		     ;;   '( ("/INBOX"               . ?i)
-;; 		     ;; 	  ("/[Gmail].Sent Mail"   . ?s)
-;; 		     ;; 	  ("/[Gmail].Trash"       . ?t)
-;; 		     ;; 	  ("/[Gmail].All Mail"    . ?a))
-;;  		     ( mu4e-compose-signature .
-;;  					      (concat
-;;  					       "Cheers,\n"
-;;  					       "Hans-Gunther Schmidt\n"))))
-;;  	 ,(make-mu4e-context
-;;  	   :name "Work"
-;;  	   :enter-func (lambda () (mu4e-message "Switch to the Work context"))
-;;  	   ;; leave-fun not defined
-;;  	   :match-func (lambda (msg)
-;;  			 (when msg 
-;;  			   (mu4e-message-contact-field-matches msg 
-;;  							       :to "hans.schmidt@meltwater.com")))
-;;  	   :vars '(  ( user-mail-address	     . "hans.schmidt@meltwater.com" )
-;;  		     ( user-full-name	    . "Hans-Gunther Schmidt" )
-;;  		     ( mu4e-compose-signature .
-;;  					      (concat
-;;  					       "Hans-Gunther Schmidt\n"
-;;  					       "Meltwater Berlin\n"))))))
-
-;; set `mu4e-context-policy` and `mu4e-compose-policy` to tweak when mu4e should
-;; guess or ask the correct context, e.g.
-
-;; start with the first (default) context; 
-;; default is to ask-if-none (ask when there's no context yet, and none match)
-;; (setq mu4e-context-policy 'pick-first)
-
-;; compose with the current context is no context matches;
-;; default is to ask 
-;; '(setq mu4e-compose-context-policy nil)
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,11 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+    (package-initialize))
+
 (load "~/.emacs.d/email")
 (load "~/.emacs.d/themes")
 
