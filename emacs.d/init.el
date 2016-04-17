f9b6fb13ed0d169fc0166c46607dcd88141af78c
Hans-Gunther Schmidt <hans@otype.de>
2016-04-17 20:44:03 +0200

    Add backup configuration to emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -14,9 +14,22 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+;; automatically follow symlinks
+(setq vc-follow-symlinks t)
+
 ;; Check any TLS connections against my local trust roots
 (setq tls-checktrust t)
 (setq gnutls-verify-error t)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+(setq backup-by-copying t)
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
