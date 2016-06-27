f43b489e4637826163fdaad9af3473ff22e6163e
Hans-Gunther Schmidt <hans.schmidt@meltwater.com>
2016-06-27 10:38:42 +0200

    Add scala module to spacemacs

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -32,6 +32,7 @@ values."
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
+     scala
      spell-checking
      syntax-checking
      version-control
