198a0c53b8580f438d52ae3f28ef4521952bf471
Hans-Gunther Schmidt <hans.schmidt@meltwater.com>
2016-08-16 09:17:26 +0200

    Disable vc-follow-symlink

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -253,8 +253,6 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  ;; automatically follow symlinks on open
-  (setq vc-follow-symlinks t)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
