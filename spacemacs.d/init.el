aa82b846dcf28d3691408fdbc967bb2794c471eb
Hans-Gunther Schmidt <hans.schmidt@meltwater.com>
2016-08-16 09:27:13 +0200

    Re-add moe-theme

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -98,7 +98,9 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(material
+   dotspacemacs-themes '(moe-dark
+                         moe-light
+                         material
                          afternoon
                          niflheim
                          planet
@@ -106,6 +108,8 @@ values."
                          sanityinc-tomorrow-eighties
                          tangotango
                          tango-dark)
+   ;; set moe-theme color
+   moe-theme-set-color 'yellow
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
