f7cef9bd9b6ed0b878b8a4f076304c68af554138
Hans-Gunther Schmidt <hans.schmidt@meltwater.com>
2016-08-19 11:06:30 +0200

    Set neotree-projectile-action on projectile switch

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -266,6 +266,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (setq projectile-switch-project-action 'neotree-projectile-action)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
