ba3da1240b456b45b25b55760438691465e325dc
Hans-Gunther Schmidt <hans.schmidt@meltwater.com>
2016-10-12 14:23:45 +0200

    Remove custom package definition

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -277,3 +277,9 @@ you should place your code here."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (quote ("~/org/mw-tasks.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
