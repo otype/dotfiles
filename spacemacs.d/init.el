48030ebfb030a1915a567fc3cfa421fd0bd4a3c3
Hans-Gunther Schmidt <hans.schmidt@meltwater.com>
2016-12-05 09:05:54 +0100

    Remove failing layers in spacemacs

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -28,12 +28,11 @@ values."
      clojure
      colors
      dash
-     dockerfile
+     docker
      elixir
      emacs-lisp
      erlang
      git
-     groovy
      html
      javascript
      lua
@@ -290,4 +289,4 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
