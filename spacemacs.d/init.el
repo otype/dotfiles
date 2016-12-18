286a72597b8bc8fbbd413b37c110d8d96ee4e3e0
Hans-Gunther Schmidt <hans@otype.de>
2016-12-18 15:45:20 +0100

    Restrict spacemacs themes

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -112,8 +112,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(tango-dark
-                         tango-plus
+   dotspacemacs-themes '(tango-plus
                          flatland
                          moe-dark
                          moe-light)
