a736a8b9fac2b3ab6a1cbdd189a7842e3212e189
Hans-Gunther Schmidt <hans.schmidt@meltwater.com>
2017-01-05 13:58:37 +0100

    Remove unused configuration files

--- my_i3status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# shell script to prepend i3status with more stuff
-i3status | while :
-do
-    read line
-    echo "`readlink /usr/local/etc/openvpn/openvpn.conf | cut -d. -f1`->$line" || exit 1
-done
