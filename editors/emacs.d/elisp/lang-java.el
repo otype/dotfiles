381b96fc65ef7465cb559538fb16dab32777c98c
Hans-Gunther Schmidt <hans@otype.de>
2019-05-20 09:33:15 +0200

    Remove emacs and zsh configuration as these have moved to own repositories

--- editors/emacs.d/elisp/lang-java.el
+++
@@ -1,7 +0,0 @@
-(use-package jdee
-  :defer t
-  :config
-  (add-hook 'java-mode-hook 'jdee-mode)
-  (custom-set-variables '(jdee-server-dir (substitute-in-file-name "${HOME}/.emacs.d/jdee-server"))))
-
-(provide 'lang-java)
